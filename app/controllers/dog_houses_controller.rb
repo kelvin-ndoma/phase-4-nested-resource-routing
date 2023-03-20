fcbcfd5150a744d089ba59d2e134afd09d956948
kevin.mutua@student.moringaschool.com <kevin.mutua@student.moringaschool.com>
2023-03-20 14:23:55 +0300

    completed

--- app/controllers/dog_houses_controller.rb
+++ app/controllers/dog_houses_controller.rb
@@ -5,7 +5,25 @@ class DogHousesController < ApplicationController
     dog_house = DogHouse.find(params[:id])
     render json: dog_house
   end
+  def reviews_index
+    dog_house = DogHouse.find(params[:dog_house_id])
+    reviews = dog_house.reviews
+    render json: reviews, include: :dog_house
+  end
 
+  def review
+    review = Review.find(params[:id])
+    render json: review, include: :dog_house
+  end
+  def index
+    if params[:dog_house_id]
+      dog_house = DogHouse.find(params[:dog_house_id])
+      reviews = dog_house.reviews
+    else
+      reviews = Review.all
+    end
+    render json: reviews, include: :dog_house
+  end
   private
 
   def render_not_found_response
